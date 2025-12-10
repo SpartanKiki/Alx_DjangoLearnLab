ca19ad131c1dcc795757adf86848bdef834e69d3
SpartanKiki <nyaga6178@gmail.com>
2025-12-10 12:02:38 +0300

    Fix Task 0: Move relationship_app to correct location, add models and query_samples

--- Introduction_to_Django/django-models/relationship_app/models.py
+++ Introduction_to_Django/django-models/relationship_app/models.py
@@ -1,32 +1,30 @@
 from django.db import models
 
-# Author model
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Book model
+
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publication_year = models.IntegerField(null=True, blank=True)  # optional field
 
     def __str__(self):
         return self.title
 
-# Library model
+
 class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, blank=True)
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-# Librarian model
+
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
--- Introduction_to_Django/django-models/relationship_app/query_samples.py
+++ Introduction_to_Django/django-models/relationship_app/query_samples.py
@@ -1,19 +1,13 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
-def sample_queries():
-    # Query all books by a specific author
-    author = Author.objects.first()
-    if author:
-        books_by_author = Book.objects.filter(author=author)
-        print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+def query_all_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
-    # List all books in a library
-    library = Library.objects.first()
-    if library:
-        books_in_library = library.books.all()
-        print(f"Books in library {library.name}: {[book.title for book in books_in_library]}")
+def list_all_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-    # Retrieve the librarian for a library
-    if library and hasattr(library, 'librarian'):
-        librarian = library.librarian
-        print(f"Librarian of {library.name}: {librarian.name}")
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
