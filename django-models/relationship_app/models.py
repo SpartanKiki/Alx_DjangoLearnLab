7a7f68124b6ffbf3f5be4e26af821982d9749e3a
SpartanKiki <nyaga6178@gmail.com>
2025-12-27 08:24:05 -0500

    Task 0: Implement advanced model relationships in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,32 +1,47 @@
 from django.db import models
 
-
+# Author model
+# One author can write many books
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
+# Book model
+# Each book belongs to ONE author (ForeignKey)
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
 
     def __str__(self):
         return self.title
 
 
+# Library model
+# A library can have MANY books and a book can be in MANY libraries
 class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
 
 
+# Librarian model
+# Each library has ONE librarian and each librarian manages ONE library
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        related_name='librarian'
+    )
 
     def __str__(self):
         return self.name
