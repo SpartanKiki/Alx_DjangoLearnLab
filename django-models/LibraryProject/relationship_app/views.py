7d406f1e0cbdd5152832e3a10c66dc597d76a492
SpartanKiki <nyaga6178@gmail.com>
2025-12-27 11:31:05 -0500

    Task 1: Implement FBV and CBV with proper queries and URL routing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
-
-# Function-based view
+# Function-Based View
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-# Class-based view
+# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
