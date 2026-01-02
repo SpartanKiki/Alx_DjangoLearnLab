338414f1729b0390d2a896ab56cb7063fb346c46
SpartanKiki <nyaga6178@gmail.com>
2026-01-02 04:13:52 -0500

    Fix Task 2: Auth views, URLs, and templates in correct location

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    UserLoginView,
-    UserLogoutView,
-    register
-)
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # Task 1 views
+    path('books/', views.book_list, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    # Task 2 authentication views
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
-
