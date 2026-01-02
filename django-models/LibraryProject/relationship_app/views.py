75f831c7c57b7f6ccfc0984c192742f81345d15f
SpartanKiki <nyaga6178@gmail.com>
2026-01-01 20:24:07 -0500

    Task 1: Fix FBV, CBV, URLs, and templates in checker path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,31 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
-# Function-Based View
+
 def list_books(request):
     books = Book.objects.all()
+    output = []
+    for book in books:
+        output.append(f"{book.title} by {book.author.name}")
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# Function-Based View
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-# Class-Based View
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
