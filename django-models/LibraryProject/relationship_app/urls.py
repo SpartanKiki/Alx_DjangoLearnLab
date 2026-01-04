17152d009b4fa557043407ab47e0e4eded870477
SpartanKiki <nyaga6178@gmail.com>
2026-01-04 12:52:43 -0500

    Task 3: Implement role-based access control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # Task 1 views
-    path('books/', views.book_list, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    # Task 2 authentication views
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,40 +1,29 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from django.contrib.auth.decorators import user_passes_test
 
 
-def list_books(request):
-    books = Book.objects.all()
-    output = []
-    for book in books:
-        output.append(f"{book.title} by {book.author.name}")
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
 
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect, render
 
-class UserLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
 
-class UserLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
