193164cbb9681e22a81632125f1b06c33e6b6264
SpartanKiki <nyaga6178@gmail.com>
2026-01-04 13:07:37 -0500

    Task 4: Implement custom permissions for Book model

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -27,3 +27,49 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+from .models import Book
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            published_date=published_date
+        )
+        return redirect('add_book')
+
+    return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.published_date = request.POST.get('published_date')
+        book.save()
+        return redirect('edit_book', book_id=book.id)
+
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('add_book')
+
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
