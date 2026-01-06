2e8b9e01b4e49a4690757ac45c566e234994ba49
SpartanKiki <nyaga6178@gmail.com>
2026-01-06 09:18:37 +0300

    Add bookshelf models.py for Task 0 compliance

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
