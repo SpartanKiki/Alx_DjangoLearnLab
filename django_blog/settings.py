8daa6dc7cea186b696903b51b4aa1c0cc76f0664
SpartanKiki <nyaga6178@gmail.com>
2026-01-15 11:55:12 +0300

    Task 0: Fixed initial setup and Post model

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # <<< add this
+
+    'blog',  # <- this must be here
 ]
+
 
 
 MIDDLEWARE = [
