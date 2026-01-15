2eac3c84d7fc4e396412d8a95465a73f8d6e7553
SpartanKiki <nyaga6178@gmail.com>
2026-01-15 15:52:10 +0300

    Restore Comment model and fix import error

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,28 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
 
     def __str__(self):
         return self.title
 
-from django.db import models
-from django.contrib.auth.models import User
-from django.urls import reverse
+    def get_absolute_url(self):
+        return reverse("post-detail", args=[self.pk])
+
 
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Comment by {self.author.username} on {self.post.title}'
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.post.pk})
+        return f"Comment by {self.author} on {self.post}"
